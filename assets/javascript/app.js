b29ddc37376e1d554adba04f0e29f7d55289caae
Jean Tzintzun <jtzintzun@Jeans-MacBook.local>
2017-12-19 16:54:42 -0500

    countdown

---
+++ assets/javascript/app.js
@@ -0,0 +1,32 @@
+// 
+//   //  Start on click.
+// var seconds = 10
+// var numberOfGames = 0;
+// $("#start").on("click", function() {
+// //    var seconds = 5;
+//     countDown(seconds)
+//   });
+//
+//   function countDown(seconds) {
+//     $("#remainingTime").html("remainig time: " + seconds);
+//     if (seconds < 0) {
+//       clearTimeout(timer);
+//       $("#remainingTime").html("No remainig time");
+//       vartimer2 = setTimeout('correctAnsw()',1500);
+//       return
+//     }
+//     seconds--;
+//     var timer = setTimeout('countDown(' + seconds + ')', 1000);
+//   //  console.log(seconds);
+//   }
+//
+// function correctAnsw (){
+// $("#remainingTime").html("correct Answe");
+// var timer = setTimeout('countDown(' + seconds + ')', 5000);
+// }
+//
+// function gameOver(){
+// $("#remainingTime").html("GAME OVER")
+// debugger
+// return;
+// }
